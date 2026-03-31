413bc6b5897e2bf4445d6b306f0638f57e788c22
Mike Sanders <mikesndrs@gmail.com>
2026-03-31 14:48:16 +0200

    docstrings for log file viewer

--- muscle3_dashboard/components/log_files.py
+++ muscle3_dashboard/components/log_files.py
@@ -1,9 +1,14 @@
+from collections import defaultdict
+
 import panel as pn
 
 from muscle3_dashboard.constants import CARD_MARGIN
 
 
 class LogFilesViewer(pn.viewable.Viewer):
+    """Panel component showing the log files for the muscle manager and the\
+    separate components"""
+
     def __init__(self) -> None:
         super().__init__()
         self.aggregated_tab = self.aggregated_tab_pane()
@@ -21,12 +26,15 @@ class LogFilesViewer(pn.viewable.Viewer):
         self.card = pn.Card(self.tabs, margin=CARD_MARGIN, title="Log files")
 
     def aggregated_tab_pane(self):
+        """Tab for aggregated logs"""
         return self.log_pane()
 
     def muscle_manager_tab_pane(self):
+        """Tab for muscle manager log"""
         return self.log_pane()
 
     def components_tab_pane(self):
+        """Tab for separate component logs"""
         self.component_terminals = {}
         self.select = pn.widgets.Select(name="Choose log", groups={})
         self.terminal_container = pn.Column()
@@ -34,12 +42,14 @@ class LogFilesViewer(pn.viewable.Viewer):
         return pn.Column(self.select, self.terminal_container)
 
     def update_component_logs(self, event):
+        """Update component logs on trigger"""
         if event.new not in self.component_terminals:
             self.component_terminals[event.new] = self.log_pane()
         self.terminal_container.clear()
         self.terminal_container.append(self.component_terminals[event.new])
 
     def log_pane(self):
+        """Get basic terminal"""
         return pn.widgets.Terminal(
             "",
             sizing_mode="stretch_both",
@@ -52,6 +62,7 @@ class LogFilesViewer(pn.viewable.Viewer):
         stdout_log_lines=None,
         stderr_log_lines=None,
     ):
+        """Method to update log file viewer state from outside"""
         if manager_log_lines is not None:
             for line in manager_log_lines:
                 self.muscle_manager_tab.write(line)
@@ -71,8 +82,6 @@ class LogFilesViewer(pn.viewable.Viewer):
                     self.component_terminals[key] = self.log_pane()
                 for line in lines:
                     self.component_terminals[key].write(line)
-
-        from collections import defaultdict
 
         groups = defaultdict(list)
         for key in self.component_terminals:
